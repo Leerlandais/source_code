dafe3be788da39f09c937103b375af30f988bbeb
leerlandais <leerlandais@gmail.com>
2024-10-05 19:19:26 +0200

    found the problem with URL production - Use double quotes for declarations and single quotes for commands in Bash

--- Bash/fork-project.bash
+++ Bash/fork-project.bash
@@ -1,9 +1,18 @@
+https://github.com/Leerlandais/source_code.git
 
-read -p "Enter the Project Name: " PROJ_NAME
+
 read -p "Copy the URL of YOUR fork here: " FORK_DIR
+PROJ_NAME=$(basename "$FORK_DIR" .git)
+echo 'The project will be called:' $PROJ_NAME && \
 read -p "Enter the database name: " DB_NAME
 
-git clone "$FORK_DIR" && \
-cd "$PROJ_NAME" && \
-echo "$DB_NAME"
 
+echo 'Cloning the repository' && \
+git clone "$FORK_DIR" && \
+echo 'Cloning Complete' && \
+cd "$PROJ_NAME" && \
+
+UPSTR_URL="https://github.com/Leerlandais/$PROJ_NAME.git"
+echo "Adding Upstream address as :" $UPSTR_URL && \
+git remote add upstream "$UPSTR_URL" && \
+echo "Upstream Remote Added"

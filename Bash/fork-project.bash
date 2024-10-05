9a06773c385fd94a514ed6270c6a2cef0d2d35e6
leerlandais <leerlandais@gmail.com>
2024-10-05 19:32:35 +0200

    finished the fork-project

--- Bash/fork-project.bash
+++ Bash/fork-project.bash
@@ -16,3 +16,16 @@ UPSTR_URL="https://github.com/Leerlandais/$PROJ_NAME.git"
 echo "Adding Upstream address as :" $UPSTR_URL && \
 git remote add upstream "$UPSTR_URL" && \
 echo "Upstream Remote Added"
+
+cat <<EOF > config.php
+<?php
+const DB_DRIVER = "mysql";
+const DB_HOST = "localhost";
+const DB_LOGIN = "root";
+const DB_PWD = "";
+const DB_NAME = "${DB_NAME}";
+const DB_PORT = 3306;
+const DB_CHARSET = "utf8mb4";
+
+const PROJECT_DIRECTORY = __DIR__;
+EOF

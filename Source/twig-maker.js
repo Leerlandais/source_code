f9678f283bccf14a6ad2a6f2f160555005ac991a
leerlandais <leerlandais@gmail.com>
2024-10-11 22:19:47 +0200

    good up to the big index - nearly there

--- Source/twig-maker.js
+++ Source/twig-maker.js
@@ -151,15 +151,72 @@ switch ($route) {
 {# The Base Twig #}        
         `;
         fs.writeFileSync(`${projName}/view/base.html.twig`, baseTwig);
+
         // template.twig
         const tempTwig = `
 {% extends 'base.twig.html' %}   
         `;
         fs.writeFileSync(`${projName}/view/template.html.twig`, tempTwig);
+
         // homepage.twig
+        const homeTwig = `
+{% extends 'template.twig.html' %}   
+        `;
+        fs.writeFileSync(`${projName}/view/public/public.index.html.twig`, homeTwig);
 
         // and the big index
+        const pubIndex = `
+<?php
+use Twig\\Loader\\FilesystemLoader;
+use Twig\\Environment;
 
+require_once "../config.php";
+
+spl_autoload_register(function ($class) {
+  $class = str_replace('\\\\', '/', $class);
+  require PROJECT_DIRECTORY.'/' .$class . '.php';
+});
+
+require_once PROJECT_DIRECTORY.'/vendor/autoload.php';
+
+$loader = new FilesystemLoader(PROJECT_DIRECTORY.'/view/');
+
+// Dev version
+$twig = new Environment($loader, [
+  'debug' => true,
+]);
+$twig->addExtension(new \\Twig\\Extension\\DebugExtension());
+
+$twig->addGlobal('PUBLIC_DIR', PUB_DIR);
+$twig->addGlobal('PROJECT_DIR', PROJECT_DIRECTORY);
+
+/*
+// Prod version
+$twig = new Environment($loader, [
+   'cache' => '../cache/Twig',
+   'debug' => false,
+]);
+// no DebugExtension online
+*/
+
+
+/* NO DB FOR THIS PROJECT (AT THE CURRENT TIME)
+try {
+   $db = MyPDO::getInstance(DB_DRIVER . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";port=" . DB_PORT . ";charset=" . DB_CHARSET,
+       DB_LOGIN,
+       DB_PWD);
+   $db->setAttribute(MyPDO::ATTR_ERRMODE, MyPDO::ERRMODE_EXCEPTION);
+   $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+}catch (Exception $e){
+   die($e->getMessage());
+}
+
+*/
+require_once PROJECT_DIRECTORY.'/controller/routerController.php';
+
+// $db = null;   
+        `;
+        fs.writeFileSync(`${projName}/public/index.php`, pubIndex);
         // run composer (make sure I'm in the folder)
 
         // add git and create the glory commit

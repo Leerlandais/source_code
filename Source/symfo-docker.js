11ebaea4951c03c26062e8750393b9d3cc2a0f75
leerlandais <leerlandais@gmail.com>
2024-10-25 09:27:38 +0200

    started a symDocker RL

---
+++ Source/symfo-docker.js
@@ -0,0 +1,31 @@
+const readline = require('readline');
+const { execSync } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const completed = (msg) => {
+    console.log(msg);
+    rl.question('The process seems to have completed successfully - press Enter to close', () => {
+        rl.close();
+    });
+};
+rl.question("Enter the project name: ", function(projName) {
+    try {
+
+
+
+
+    }catch (error) {
+        console.error(`Error occurred: ${error.message}`);
+    }
+
+        completed("Setup completed successfully!");
+
+});
+
+// pkg Source/symfo-docker.js --targets node18-win-x64 --output symfoDocker.exe

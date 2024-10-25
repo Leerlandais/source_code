90dc63065e79775f00f0025e6f243d1b69284473
leerlandais <leerlandais@gmail.com>
2024-10-25 09:37:57 +0200

    added base questions

--- Source/symfo-docker.js
+++ Source/symfo-docker.js
@@ -15,17 +15,22 @@ const completed = (msg) => {
     });
 };
 rl.question("Enter the project name: ", function(projName) {
-    try {
+    rl.question("Enter the database name (DB_NAME): ", function(dbName) {
+        rl.question("Enter the nginx port (e.g. 8080:80): ", function(ngPort) {
+            rl.question("Enter the mysql port (e.g. 3308:3306): ", function(myPort) {
+                rl.question("Enter the phpmyadmin port (e.g. 8081:80): ", function(phpPort) {
+        try {
 
 
-
-
-    }catch (error) {
-        console.error(`Error occurred: ${error.message}`);
-    }
+        } catch (error) {
+            console.error(`Error occurred: ${error.message}`);
+        }
 
         completed("Setup completed successfully!");
-
-});
+                }); // end phpmyadmin question
+            }); // end mysql question
+        }); // end nginx question
+    }); // end db_name question
+}); // end proj_name question
 
 // pkg Source/symfo-docker.js --targets node18-win-x64 --output symfoDocker.exe

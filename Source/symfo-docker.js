6e4d56701bf5566d3911fae9861ad23d8f64e341
leerlandais <leerlandais@gmail.com>
2024-10-31 14:07:13 +0100

    updated for multi env

--- Source/symfo-docker.js
+++ Source/symfo-docker.js
@@ -61,6 +61,7 @@ RUN apt-get update && apt-get install -y git
 # Created .bashrc to set up an alias for php bin/yadayada (I'm sick of typing that :-D)
 RUN echo "alias lee='php bin/console'" >> /root/.bashrc
 RUN echo "alias cr='composer require'" >> /root/.bashrc
+RUN echo "alias ci='composer require'" >> /root/.bashrc
 
 # Configurer Xdebug
 RUN echo "xdebug.mode=debug" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini \\
@@ -78,7 +79,7 @@ RUN docker-php-ext-install opcache \\
     && echo "opcache.revalidate_freq=0" >> /usr/local/etc/php/conf.d/opcache.ini \\
     && echo "opcache.validate_timestamps=1" >> /usr/local/etc/php/conf.d/opcache.ini`
 
-            fs.writeFileSync('Dockerfile', dockFile);
+            fs.writeFileSync('Dockerfile.bak', dockFile);
 
             console.log('Dockerfile creation complete');
 
@@ -109,7 +110,9 @@ RUN docker-php-ext-install opcache \\
 }`
         fs.writeFileSync(`default.conf`, ngFile);
         console.log("nginx folder and default.conf created")
+
             process.chdir('..');
+
 
         console.log("Creating docker-compose");
         const dockComp = `
@@ -169,10 +172,10 @@ networks:
 
 
 `
-        fs.writeFileSync('docker-compose.yaml', dockComp);
+        fs.writeFileSync('docker-compose.yaml.bak', dockComp);
         console.log('Docker-compose.yaml created');
 
-        console.log('Beginning creation of .env file');
+        console.log('Beginning creation of .env file for Symfony');
         const env = `# In all environments, the following files are loaded if they exist,
 # the latter taking precedence over the former:
 #
@@ -207,7 +210,7 @@ PMA_PASS=password
 # IMPORTANT: You MUST configure your server version, either here or in config/packages/doctrine.yaml
 #
 # DATABASE_URL="sqlite:///%kernel.project_dir%/var/data.db"
-DATABASE_URL="mysql://user:password@mysql:3306/${dbName}?serverVersion=8.0.32&charset=utf8mb4"
+DATABASE_URL="mysql://root:@127.0.0.1:3306/${dbName}?serverVersion=8.0.32&charset=utf8mb4"
 # DATABASE_URL="mysql://app:!ChangeMe!@127.0.0.1:3306/app?serverVersion=10.11.2-MariaDB&charset=utf8mb4"
 # DATABASE_URL="postgresql://app:!ChangeMe!@127.0.0.1:5432/app?serverVersion=16&charset=utf8"
 ###< doctrine/doctrine-bundle ###
@@ -225,19 +228,73 @@ MESSENGER_TRANSPORT_DSN=doctrine://default?auto_setup=0
             fs.writeFileSync('.env', env);
         console.log("env file created");
 
+            console.log('Beginning creation of .env file for Docker');
+            const envDock = `# In all environments, the following files are loaded if they exist,
+# the latter taking precedence over the former:
+#
+#  * .env                contains default values for the environment variables needed by the app
+#  * .env.local          uncommitted file with local overrides
+#  * .env.$APP_ENV       committed environment-specific defaults
+#  * .env.$APP_ENV.local uncommitted environment-specific overrides
+#
+# Real environment variables win over .env files.
+#
+# DO NOT DEFINE PRODUCTION SECRETS IN THIS FILE NOR IN ANY OTHER COMMITTED FILES.
+# https://symfony.com/doc/current/configuration/secrets.html
+#
+# Run "composer dump-env prod" to compile .env files for production use (requires symfony/flex >=1.2).
+# https://symfony.com/doc/current/best_practices.html#use-environment-variables-for-infrastructure-configuration
+
+###> symfony/framework-bundle ###
+APP_ENV=dev
+APP_SECRET=6639ee7c63b4c0ccbacb295990261ac3
+###< symfony/framework-bundle ###
+
+MYSQL_ROOT=root
+MYSQL_DB=${dbName}
+MYSQL_USER=user
+MYSQL_PASS=password
+PMA_HOST=mysql
+PMA_USER=user
+PMA_PASS=password
+
+###> doctrine/doctrine-bundle ###
+# Format described at https://www.doctrine-project.org/projects/doctrine-dbal/en/latest/reference/configuration.html#connecting-using-a-url
+# IMPORTANT: You MUST configure your server version, either here or in config/packages/doctrine.yaml
+#
+# DATABASE_URL="sqlite:///%kernel.project_dir%/var/data.db"
+DATABASE_URL="mysql://\${PMA_USER}:\${PMA_PASS}@\${PMA_HOST}:3306/${dbName}?serverVersion=8.0.32&charset=utf8mb4"
+# DATABASE_URL="mysql://app:!ChangeMe!@127.0.0.1:3306/app?serverVersion=10.11.2-MariaDB&charset=utf8mb4"
+# DATABASE_URL="postgresql://app:!ChangeMe!@127.0.0.1:5432/app?serverVersion=16&charset=utf8"
+###< doctrine/doctrine-bundle ###
+
+###> symfony/messenger ###
+# Choose one of the transports below
+# MESSENGER_TRANSPORT_DSN=amqp://guest:guest@localhost:5672/%2f/messages
+# MESSENGER_TRANSPORT_DSN=redis://localhost:6379/messages
+MESSENGER_TRANSPORT_DSN=doctrine://default?auto_setup=0
+###< symfony/messenger ###
+
+###> symfony/mailer ###
+# MAILER_DSN=null://null
+###< symfony/mailer ###`
+            fs.writeFileSync('.env.bak', envDock);
+            console.log("env file created");
+
+
         execSync('git branch -m main');
         execSync('git add .');
         execSync('git commit -m "project setup automated by https://leerlandais.com"');
 
+/*
             console.log("deleting unnecessary files");
-
             fs.unlink('compose.yaml', (err) => {
                 if (err) throw err;
             });
             fs.unlink('compose.override.yaml', (err) => {
                 if (err) throw err;
             });
-
+*/
         if(gitRep) {
             execSync(`git remote add origin ${gitRep}`, {stdio : "inherit"});
             execSync('git push -u origin main', {stdio : "inherit"});

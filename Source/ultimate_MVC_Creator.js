eef721ae7877ffb2939ba68834c62eba00ee6a7b
leerlandais <leerlandais.com>
2025-03-26 17:59:01 +0100

    corrected error in myPDO

--- Source/ultimate_MVC_Creator.js
+++ Source/ultimate_MVC_Creator.js
@@ -263,28 +263,28 @@ $db = null;`;
                     fs.writeFileSync(`${projName}/public/index.php`, pubIndex);
 
                     const pdo = `<?php
-space model;
-PDO;
-Exception;
-PDOStatement;
-s MyPDO extends PDO
-
-private static ?MyPDO $instance = null;
-private function __construct($dsn, $username = null, $password = null, $options = null)
+namespace model;
+use PDO;
+use Exception;
+use PDOStatement;
+class MyPDO extends PDO
 {
-    parent::__construct($dsn, $username, $password, $options);
-}
-public static function getInstance($dsn, $username = null, $password = null, $options = null): MyPDO
-{
-    if (self::$instance === null) {
-        try {
-            self::$instance = new MyPDO($dsn, $username, $password, $options);
-        } catch (Exception $e) {
-            die("Erreur de connexion : " . $e->getMessage());
-        }
+    private static ?MyPDO $instance = null;
+    private function __construct($dsn, $username = null, $password = null, $options = null)
+    {
+        parent::__construct($dsn, $username, $password, $options);
     }
-    return self::$instance;
-}
+    public static function getInstance($dsn, $username = null, $password = null, $options = null): MyPDO
+    {
+        if (self::$instance === null) {
+            try {
+                self::$instance = new MyPDO($dsn, $username, $password, $options);
+            } catch (Exception $e) {
+                die("Erreur de connexion : " . $e->getMessage());
+            }
+        }
+        return self::$instance;
+    }
 }`;
                     fs.writeFileSync(`${projName}/model/MyPDO.php`, pdo);
 
